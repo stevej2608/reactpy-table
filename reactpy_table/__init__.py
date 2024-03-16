211fac58eaefaecd445b0b108dcf16ca286b5b38
Steve Jones <jonesst2608@gmail.com>
2024-03-16 16:20:08 +0000

    Bump version (0.0.12)

--- reactpy_table/__init__.py
+++ reactpy_table/__init__.py
@@ -4,4 +4,4 @@
 from .core.reactpy_table_init import Options, Table, use_reactpy_table
 from .types import ColumnDef, Columns, IPaginator, ITableSearch, TData
 
-__version__ = "0.0.11"
+__version__ = "0.0.12"
