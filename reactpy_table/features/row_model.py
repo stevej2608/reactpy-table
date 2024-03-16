9483ce0fb8c617a4ae04ef36885ca8fdb0f1b43f
Steve Jones <jonesst2608@gmail.com>
2024-03-16 15:43:39 +0000

    Row model now works

--- reactpy_table/features/row_model.py
+++ reactpy_table/features/row_model.py
@@ -1,18 +1,52 @@
-from typing import Tuple
+from typing import Tuple, Any, List
+from enum import Enum
 from ctypes import ArgumentError
+from pydantic import BaseModel
 from utils.memo import memo, MemoOpts
 from ..types import ITable, RowModel, TData, TableData, TFeatureFactory, UpstreamData, update_state
+
+class Action(Enum):
+    DELETE = 1
+    UPDATE = 2
+
+class RowAction(BaseModel):
+    action: Action | None = None
+    index: int = -1
+    row: Any | None = None
+
+NO_ACTION = RowAction()
+
 
 class DefaultRowModel(RowModel[TData]):
 
     def __init__(self, table: ITable[TData], upstream_data: UpstreamData[TData]):
         super().__init__(table, upstream_data)
 
-        def deps() -> Tuple[TableData[TData]]:
-            return (upstream_data(),)
+        self.row_actions: List[RowAction[TData]] = []
 
-        def updater(upstream_data: TableData[TData]) -> TableData[TData]:
-            return upstream_data
+        def deps() -> Tuple[TableData[TData], List[RowAction]]:
+            return (
+                upstream_data(),
+                self.row_actions.copy()
+            )
+
+        def updater(upstream_data: TableData[TData], row_actions:List[RowAction]) -> TableData[TData]:
+
+            if not row_actions:
+                return upstream_data
+
+            rows = upstream_data.rows.copy()
+
+            for action in row_actions:
+
+                if action.action == Action.DELETE:
+                    del rows[action.index]
+
+                if action.action == Action.UPDATE and action.row:
+                    rows[action.index] = action.row
+
+            return TableData(rows=rows, cols=upstream_data.cols)
+
 
         self.pipeline = memo(deps, updater, MemoOpts(name='4. DefaultRowModel'))
 
@@ -36,14 +70,14 @@ class DefaultRowModel(RowModel[TData]):
 
     @update_state
     def delete_row(self, index:int) -> None:
-        index = self.table_index(index)
-        del self.table.data.rows[index]
+        index=self.table_index(index)
+        self.row_actions.append(RowAction(action=Action.DELETE,index=index))
 
 
     @update_state
     def update_row(self, index:int, row:TData) -> None:
-        index = self.table_index(index)
-        self.table.data.rows[index] = row
+        index=self.table_index(index)
+        self.row_actions.append(RowAction(action=Action.UPDATE,index=index, row=row))
 
 
 def getDefaultRowModel() -> TFeatureFactory[TData, RowModel[TData]]:
